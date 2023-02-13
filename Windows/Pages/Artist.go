70e33beeeb16d672d900fdc4442b7442a7d07ee9
FabienArthurYnov <fabien.arthur@ynov.com>
2023-02-13 13:49:23 +0100

    custom artists pages

--- Windows/Pages/Artist.go
+++ Windows/Pages/Artist.go
@@ -13,26 +13,43 @@ import (
 
 func Artist(artist DataAPI.Artist) fyne.CanvasObject {
 	// IMAGE IMPORT
-	r, err := fyne.LoadResourceFromURLString("https://groupietrackers.herokuapp.com/api/images/imagineDragons.jpeg")
+	r, err := fyne.LoadResourceFromURLString(artist.Image)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// ITEMS ---------------------------------------------------------
-	title := canvas.NewText(artist.Name, color.Black)
-	title.TextSize = 25
+	// top text
+	titleText := canvas.NewText(artist.Name, color.Black)
+	titleText.TextSize = 25
+	title := container.New(layout.NewCenterLayout(), titleText)
+	members := canvas.NewText("", color.Black)
+	for i, member := range artist.Members {
+		members.Text += member
+		//si ce n'est pas le dernier elm
+		if i != len(artist.Members)-1 {
+			members.Text += ", "
+		}
+	}
+	members.TextSize = 20
+	TopText := container.NewVBox(title, members)
+
+	// Image
 	image1 := canvas.NewImageFromResource(r)
+	image1.FillMode = canvas.ImageFillContain
 	image2 := canvas.NewText("Map", color.Black)
 	text1 := canvas.NewText("text1", color.Black)
 	text2 := canvas.NewText("text2", color.Black)
 
-	// CONTENT -------------------------------------------------------
-	body := container.NewMax(canvas.NewRectangle(color.RGBA{R: 211, G: 211, B: 231, A: 1}), container.New(layout.NewGridLayout(5),
-		layout.NewSpacer(), layout.NewSpacer(), title, layout.NewSpacer(), layout.NewSpacer(),
-		layout.NewSpacer(), image1, layout.NewSpacer(), image2, layout.NewSpacer(),
-		layout.NewSpacer(), text1, layout.NewSpacer(), text2, layout.NewSpacer()))
-
 	//
+
+	// CONTENT -------------------------------------------------------
+	body := container.NewMax(canvas.NewRectangle(color.RGBA{R: 211, G: 211, B: 231, A: 1}),
+		container.NewBorder(
+			container.New(layout.NewCenterLayout(), TopText), nil, nil, nil,
+			container.New(layout.NewAdaptiveGridLayout(5),
+				layout.NewSpacer(), image1, layout.NewSpacer(), image2, layout.NewSpacer(),
+				layout.NewSpacer(), text1, layout.NewSpacer(), text2, layout.NewSpacer())))
 
 	return body
 }
