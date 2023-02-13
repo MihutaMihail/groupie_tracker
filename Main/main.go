a1b6123831acc8115a58cd29e22a370418838715
FabienArthurYnov <fabien.arthur@ynov.com>
2023-02-13 10:37:37 +0100

    clean main and add testwindow

--- Main/main.go
+++ Main/main.go
@@ -1,52 +1,16 @@
 package main
 
 import (
-	"Groupie-Tracker/DataAPI"
-	"fmt"
-	"image/color"
+	"Groupie-Tracker/Windows"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
 	// créé l'appli et une fenêtre
 	a := app.New()
-	w := a.NewWindow("Groupie Tracker")
-	w.Resize(fyne.NewSize(400, 400))
+	Windows.MainWindow(a)
+	Windows.TestWindow(a)
 
-	// NAVBAR
-	nav := container.NewMax(canvas.NewRectangle(color.RGBA{R: 31, G: 31, B: 35, A: 1}), canvas.NewText("navbar", color.White))
-
-	// IMAGE IMPORT
-	r, err := fyne.LoadResourceFromURLString("https://groupietrackers.herokuapp.com/api/images/imagineDragons.jpeg")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// ITEMS
-	title := canvas.NewText("title", color.Black)
-	image1 := canvas.NewImageFromResource(r)
-	image2 := canvas.NewText("image2", color.Black)
-	text1 := canvas.NewText("text1", color.Black)
-	text2 := canvas.NewText("text2", color.Black)
-
-	// CONTENT
-	body := container.NewMax(canvas.NewRectangle(color.RGBA{R: 211, G: 211, B: 231, A: 1}), container.New(layout.NewGridLayout(5),
-		layout.NewSpacer(), layout.NewSpacer(), title, layout.NewSpacer(), layout.NewSpacer(),
-		layout.NewSpacer(), image1, layout.NewSpacer(), image2, layout.NewSpacer(),
-		layout.NewSpacer(), text1, layout.NewSpacer(), text2, layout.NewSpacer()))
-
-	//
-	content := container.NewBorder(nav, nil, nil, nil, body)
-
-	// TEMP affche les datas en terminal, Mihail
-	DataAPI.GetArtistsData()
-
-	// garder à la fin ; run et affiche la fenêtre, quand elle est fermé, stop l'appli
-	w.SetContent(content)
-	w.ShowAndRun()
+	a.Run()
 }
---
+++ Windows/TestWindow.go
@@ -0,0 +1,53 @@
+package Windows
+
+import (
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func TestWindow(a fyne.App) {
+	w := a.NewWindow("TEST WINDOW")
+	w.Resize(fyne.NewSize(400, 400))
+
+	btn1 := widget.NewButton("artist1", func() {
+		log.Println("btn1")
+		// Code
+	})
+	btn2 := widget.NewButton("artist2", func() {
+		log.Println("btn2")
+		// Code
+	})
+	btn3 := widget.NewButton("artist3", func() {
+		log.Println("btn3")
+		// Code
+	})
+	btn4 := widget.NewButton("artist4", func() {
+		log.Println("btn4")
+		// Code
+	})
+	btn5 := widget.NewButton("artist5", func() {
+		log.Println("btn5")
+		// Code
+	})
+	btn6 := widget.NewButton("artist6", func() {
+		log.Println("btn6")
+		// Code
+	})
+	btn7 := widget.NewButton("artist7", func() {
+		log.Println("btn7")
+		// Code
+	})
+	btn8 := widget.NewButton("artist8", func() {
+		log.Println("btn8")
+		// Code
+	})
+
+	content := fyne.NewContainerWithLayout(layout.NewAdaptiveGridLayout(5), btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
+
+	w.SetContent(content)
+
+	w.Show()
+}
