95683e3b70d379408759fb806439d7b359d7755e
White_ONyme <112949958+DannyLap@users.noreply.github.com>
2023-02-14 11:01:00 +0100

    serarch bar

--- Windows/Pages/Navbar.go
+++ Windows/Pages/Navbar.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
@@ -12,6 +13,7 @@ import (
 )
 
 func Navbar(w fyne.Window) fyne.CanvasObject {
+	var DataTest string
 	// NAVBAR BUTTONS ------------------------------------------------
 	BtnHome := widget.NewButton("Home", func() {
 		log.Println("BtnHome")
@@ -25,16 +27,27 @@ func Navbar(w fyne.Window) fyne.CanvasObject {
 		log.Println("BtnLieux")
 		w.SetContent(container.NewBorder(Navbar(w), nil, nil, nil, Lieux()))
 	})
+	SearchText := widget.NewEntry()
+	SearchText.SetPlaceHolder("Faire une recherche")
+	BtnSubmit := widget.NewButton("Submit", func() {
+		DataTest = SearchText.Text
+		SearchText.Text = ""
+		fmt.Println(DataTest)
+
+	})
 
 	// NAVBAR ---------------------------------------------------------
 	nav := container.NewMax(canvas.NewRectangle(color.RGBA{R: 31, G: 31, B: 35, A: 1}),
-		fyne.NewContainerWithLayout(layout.NewGridLayout(7),
+		fyne.NewContainerWithLayout(layout.NewGridLayout(10),
 			layout.NewSpacer(),
 			BtnHome,
 			layout.NewSpacer(),
 			BtnArtistes,
 			layout.NewSpacer(),
 			BtnLieux,
+			layout.NewSpacer(),
+			SearchText,
+			BtnSubmit,
 			layout.NewSpacer()))
 
 	return nav
