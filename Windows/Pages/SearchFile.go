9f5377a2e9be41846343a610bd55bb4f9f9c1ac2
White_ONyme <112949958+DannyLap@users.noreply.github.com>
2023-02-27 12:46:53 +0100

    fix barre de recherche doublon quand rentrée vide

--- Windows/Pages/SearchFile.go
+++ Windows/Pages/SearchFile.go
@@ -11,24 +11,27 @@ import (
 
 func SearchBar(DataSearchBar string, w fyne.Window) fyne.CanvasObject {
 	artists := DataAPI.GetArtistsData()
-
 	listContainer := fyne.NewContainerWithLayout(layout.NewAdaptiveGridLayout(3))
 
-	for _, artist := range artists {
-		if strings.ToLower(artist.Name) == strings.ToLower(DataSearchBar) { // Cas ou le nom est exactememnt pareil
-			fmt.Println("Trouvé cet unique artiste" + artist.Name)
-			listContainer = fyne.NewContainer(fyne.CanvasObject(Artist(artist)))
+	if len(DataSearchBar) == 0 {
+		return ArtistList(w)
+	} else {
+		for _, artist := range artists {
+			if strings.ToLower(artist.Name) == strings.ToLower(DataSearchBar) { // Cas ou le nom est exactememnt pareil
+				fmt.Println("Trouvé cet unique artiste" + artist.Name)
+				listContainer = fyne.NewContainer(fyne.CanvasObject(Artist(artist)))
 
-		} else if len(DataSearchBar) <= len(artist.Name) { // cas ou le terme cherhcer est plus cours que les noms
-			for i := 0; i < len(artist.Name)-len(DataSearchBar)+1; i++ { //-len(DataSearchBar)
-				if strings.ToLower(artist.Name[i:i+len(DataSearchBar)]) == strings.ToLower(DataSearchBar) {
-					fmt.Println("Trouvé cet artist " + artist.Name)
+			} else if len(DataSearchBar) <= len(artist.Name) { // cas ou le terme cherhcer est plus cours que les noms
+				for i := 0; i < len(artist.Name)-len(DataSearchBar)+1; i++ { //-len(DataSearchBar)
+					if strings.ToLower(artist.Name[i:i+len(DataSearchBar)]) == strings.ToLower(DataSearchBar) {
+						fmt.Println("Trouvé cet artist " + artist.Name)
 
-					btn := widget.NewButton(artist.Name, nil)
-					btn.OnTapped = func() {
-						FindArtist(btn.Text, artists, w)
+						btn := widget.NewButton(artist.Name, nil)
+						btn.OnTapped = func() {
+							FindArtist(btn.Text, artists, w)
+						}
+						listContainer.Add(btn)
 					}
-					listContainer.Add(btn)
 				}
 			}
 		}
