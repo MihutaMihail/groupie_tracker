7fa283c4ea57157340c75565a6d30f2de1665eef
White_ONyme <112949958+DannyLap@users.noreply.github.com>
2023-02-27 09:55:43 +0100

    Serach Bar Case sensitive

--- Windows/Pages/SearchFile.go
+++ Windows/Pages/SearchFile.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"strings"
 )
 
 func SearchBar(DataSearchBar string, w fyne.Window) fyne.CanvasObject {
@@ -14,14 +15,13 @@ func SearchBar(DataSearchBar string, w fyne.Window) fyne.CanvasObject {
 	listContainer := fyne.NewContainerWithLayout(layout.NewAdaptiveGridLayout(3))
 
 	for _, artist := range artists {
-		if artist.Name == DataSearchBar { // Cas ou le nom est exactememnt pareil
+		if strings.ToLower(artist.Name) == strings.ToLower(DataSearchBar) { // Cas ou le nom est exactememnt pareil
 			fmt.Println("Trouvé cet unique artiste" + artist.Name)
-			//FindArtist(DataSearchBar, artists)
-			//TODO afficher la fenetre
+			listContainer = fyne.NewContainer(fyne.CanvasObject(Artist(artist)))
 
 		} else if len(DataSearchBar) <= len(artist.Name) { // cas ou le terme cherhcer est plus cours que les noms
 			for i := 0; i < len(artist.Name)-len(DataSearchBar)+1; i++ { //-len(DataSearchBar)
-				if artist.Name[i:i+len(DataSearchBar)] == DataSearchBar {
+				if strings.ToLower(artist.Name[i:i+len(DataSearchBar)]) == strings.ToLower(DataSearchBar) {
 					fmt.Println("Trouvé cet artist " + artist.Name)
 
 					btn := widget.NewButton(artist.Name, nil)
