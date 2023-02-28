0c9e6d9e533bc096ad0fd4ddb66279282ae784fc
FabienArthurYnov <112950031+FabienArthurYnov@users.noreply.github.com>
2023-02-28 08:48:42 +0100

    [MOD] lieux list now scrollable and sorted and fixed

--- Windows/Pages/LieuxList.go
+++ Windows/Pages/LieuxList.go
@@ -3,6 +3,7 @@ package pages
 import (
 	"Groupie-Tracker/DataAPI"
 	"log"
+	"sort"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -25,17 +26,25 @@ func MakeLieuxList(w fyne.Window) *fyne.Container {
 				}
 			}
 			if !isDouble {
-				btn := widget.NewButton(location, nil)
-				btn.OnTapped = func() {
-					FindLocation(btn.Text, locationsDATA, w)
-				}
-				listContainer.Add(btn)
+				allLocations = append(allLocations, location)
 			}
 			isDouble = false
 		}
 	}
 
-	return listContainer
+	sort.Strings(allLocations)
+
+	for _, location := range allLocations {
+		btn := widget.NewButton(location, nil)
+		btn.OnTapped = func() {
+			FindLocation(btn.Text, locationsDATA, w)
+		}
+		listContainer.Add(btn)
+	}
+
+	final := container.NewScroll(listContainer)
+	final.SetMinSize(fyne.NewSize(1000, 600))
+	return container.NewCenter(final)
 }
 
 func FindLocation(name string, loc []DataAPI.Location, w fyne.Window) {
